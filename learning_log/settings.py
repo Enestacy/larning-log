5fef13d22f3fd23cdfaf0980cb0743f4b1d50145
Enestacy <muzotcka@mail.ru>
2025-02-13 00:23:43 +0300

    fix: allowed hosts list

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['larning-log.onrender.com']
+ALLOWED_HOSTS = ['larning-log-j4wm.onrender.com']
 
 
 # Application definition
